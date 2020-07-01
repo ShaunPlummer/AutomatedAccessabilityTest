1feeb2e898813115fde31de5a0d2c45456b70616
Shaun Plummer <Shaunplummer247@googlemail.com>
2020-07-01 20:40:30 +0100

    feat: add layout which includes accessibility fixes
    Include an example of how the layout could be refactored to remove the
    errors but still look the same and meet material design guidelines.

--- app/src/main/java/com/washutechnologies/automatedaccessibilitytests/MainActivity.kt
+++ app/src/main/java/com/washutechnologies/automatedaccessibilitytests/MainActivity.kt
@@ -11,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Replacement layout including fixes.
+        // setContentView(R.layout.activity_main_b)
     }
 }
